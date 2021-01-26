92494dbd6b25c2edec13c3c3e6586dee342a3dd0
DrMustafaH <mustafaharith@gmail.com>
2021-01-26 15:42:43 -0800

    Implement character counter functionality

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+  // Logic to manupitale character counter in tweet and become red when exceed limit
+  $("#tweet-text").on("input", function () {
+    const myLength = $("#tweet-text").val().length;
+    let counter = $(this).siblings("div").children(".counter");
+    counter.text(140 - myLength);
+    if (myLength > 140) {
+      counter.addClass("red");
+    } else {
+      counter.removeClass("red");
+    }
+  })
+
+
+
+
+
+});

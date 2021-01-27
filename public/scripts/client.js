a4a4ce261ad78fa25c2ea73a77c7a5b91a4493bb
DrMustafaH <mustafaharith@gmail.com>
2021-01-27 12:07:05 -0800

    Implement Dynamic Tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,65 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function () {
 
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  const renderTweets = function (tweets) {
+    tweets.forEach(tweet => {
+      const result = createTweetElement(tweet);
+      $('#tweets-container').append(result);
+    });
+  }
+
+  function createTweetElement(tweetObj) {
+    const markup =
+      `
+      <article id="tweets-container">
+    <div class="name">
+      <span class="nameNImg"><img src=${tweetObj.user.avatars}>${tweetObj.user.name}</span>
+      <span id="username">${tweetObj.user.handle}</span>
+    </div>
+    <header class="postedTweet">
+    ${tweetObj.content.text}
+      <hr class=" solidLine">
+    </header>
+    <div class="footerInfo">
+      <footer>${tweetObj.created_at}</footer>
+      <footer id="interaction">
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+      </footer>
+    </div>
+    </article>
+  `;
+    return markup;
+  }
+
+  renderTweets(data);
+})

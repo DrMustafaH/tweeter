7b95a4823558a5ffb2a6c9ccef7bfb59f19c362f
DrMustafaH <mustafaharith@gmail.com>
2021-01-28 16:31:29 -0800

    clean client.js as it is using helpers.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,10 +1,5 @@
 $(document).ready(function () {
 
-  const escape = function (str) {
-    let div = document.createElement('div');
-    div.appendChild(document.createTextNode(str));
-    return div.innerHTML;
-  }
   // logic to control the tweet button and make it form a tweet (in the server) without getting sent to the /tweets page
   const $form = $('#tweet-form')
   $form.submit(function (event) {
@@ -31,6 +26,7 @@ $(document).ready(function () {
         .then((response) => {
           loadTweets();
           $("#tweet-text").val("");
+          $(".counter").val("140");
         })
         .catch((err) => {
           error.text('⚠ Are you trying to hack me? ⚠ HAHA, try again!⚠');
@@ -38,48 +34,7 @@ $(document).ready(function () {
         })
     }
   });
-
-  // logic to render tweets submitted to and saved in the server
-  const loadTweets = function () {
-    $.get("/tweets", (response) => {
-      renderTweets(response);
-    })
-  }
+  // load tweets when page is first opened
   loadTweets()
-
-  // function that take a tweetObj and implement it on an HTML markup
-  const createTweetElement = function (tweetObj) {
-
-    const markup =
-      `
-      <article id="tweet-container">
-    <div class="name">
-      <span class="nameNImg"><img src=${tweetObj.user.avatars}>${tweetObj.user.name}</span>
-      <span id="username">${tweetObj.user.handle}</span>
-    </div>
-    <header class="postedTweet">
-    ${tweetObj.content.text}
-      <hr class=" solidLine">
-    </header>
-    <div class="footerInfo">
-      <footer>${tweetObj.created_at}</footer>
-      <footer id="interaction">
-        <i class="fas fa-flag"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="fas fa-heart"></i>
-      </footer>
-    </div>
-    </article>
-  `;
-    return markup;
-  }
-
-  // function that takes an array of tweet object and render each tweet by using prepend jquery
-  const renderTweets = function (tweets) {
-    tweets.forEach(tweet => {
-      const result = createTweetElement(tweet);
-      $('#tweets-container').prepend(result);
-    });
-  }
 
 })
